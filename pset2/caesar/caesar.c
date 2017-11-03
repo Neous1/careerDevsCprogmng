3b538f83710c43c9ddb932f2b283f2173e7b5603
Neous1 <Neous1@users.noreply.github.com>
2017-11-03 17:17:22 +0000

    modify caesar.c

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,13 +1,62 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-#include <stlib.h>
+// #include <stlib.h>
 
 // string message;
 int main(int argc, string argv[])
 {
-    // printf("here is the message");
-string message = get_string();
-printf("%s\n", message);
+    if (argc != 2){
+        printf("Please provide a positive number\n");
+    }
+    string code = argv[1];
+    int k = atoi(code);
+    
+    char c; 
+    int num;
+    printf("15 . %d\n", k);
+        printf("What is the message: ");
+    string message = get_string();
+    // printf("%s\n", message);
+    for(int i = 0, len = strlen(message);i < len; i++){
+        // printf("20 . %d\n", len);
+        printf("%c\n", message[i]);
+         num = ((message[i] -65)+ k) % 26; 
+        printf(" num is : %d\n", num);
+        c = num + 65;
+        printf("char is %c  ", c);
+    } 
 
 }
+
+
+/*
+Get the key
+if argc != 2 => throw error
+
+string code = argv[1]
+int k = atoi(code)
+
+get the plaintext
+    get_string()
+encipher
+    one character
+    entire plaintext
+print ciphertext
+
+
+
+
+get key form command line argument
+run key intor integer
+prompt for plaintextr
+for each character in the plaintext string
+    if alphabetic 
+            isalpha()
+            isupper()
+            islower()
+        preserve case
+        shift plaintext character by key
+print ciphertext
+
+*/
