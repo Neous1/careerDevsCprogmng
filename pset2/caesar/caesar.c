ce53165b0f70efa777a5494eb24bd8524f13b6fd
Neous1 <Neous1@users.noreply.github.com>
2017-11-04 13:16:59 +0000

    push character to ciphertext whether or not it's modified

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -15,37 +15,44 @@ int main(int argc, string argv[])
     string code = argv[1];
     int k = atoi(code);
     
-    char c; 
-    int num;
+    // char c; 
+    // int num;
     char letter ;
+    string ciphertext =" ";
     printf("15 .The code is:  %d\n", k);
         printf("What is the message: ");
     string message = get_string();
     // printf("%s\n", message);
     for(int i = 0, len = strlen(message);i < len; i++){
         // printf("25 . %c\n", message[i]);
+            letter = message[i];
+        if(!isalpha(letter))
+        {
+            ciphertext += letter;
+            printf("31. %s\n", ciphertext);
+        }
+            /*
         if (isalpha(message[i]))
         {
             printf("28 .%c is a letter\n", message[i]);
-            letter = message[i];
             if(isupper(letter))
             {
                 printf("32 . %c  is upper\n", letter);
                 num = ((letter -65)+ k) % 26; 
                 printf("34 .num is : %d\n", num);
                 c = num + 65;
-                printf("37char is %c  ", c);
+                printf("37 .char is %c\n  ", c);
             }
             else
             printf("40 .%c is NOT upper\n", letter);
                 num = ((letter -97)+ k) % 26; 
                 printf("42 .num is : %d\n", num);
                 c = num + 97;
-                printf("44. char is %c  ", c);            
+                printf("44. char is %c\n  ", c);            
         }
         else{
             printf("43. %c is NOT a letter \n", message[i]);
-        }
+        }*/
     } 
     // return 0;
 }
