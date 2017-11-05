34dfbe2d3d06734f71ead1eb1a913b326cc02cec
Neous1 <Neous1@users.noreply.github.com>
2017-11-05 13:17:11 +0000

    add ctype.h to handle isalpha

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // string message;
 
-
-int main(int argc, string argv[])
-{
     string isalpha();
     char isupper();
     char islower();
+
+int main(int argc, string argv[])
+{
+
     
     if (argc != 2){
         printf("Please provide a positive number\n");
@@ -21,41 +23,51 @@ int main(int argc, string argv[])
     char c; 
     int num;
     char letter;
-    // string ciphertext =" ";
+
     printf("15 .The code is:  %d\n", k);
         printf("What is the message: ");
     string message = get_string();
     // printf("%s\n", message);
     for(int i = 0, len = strlen(message);i < len; i++)
     {
+                // printf("plaintext is : %i", message[i]);
+                
+            // string ciphertext[strlen(message)];
         printf("character @ index %d is : %c\n", i,message[i]);  
         letter = message[i];
             
         if(isalpha(message[i]))
         {
             printf("34 .%c is a letter\n", message[i]);
-            if(isupper(letter))
+            if( isupper(letter))
             {
-                printf("37 . %c  is upper\n", letter);
+                // printf("37 . %c  is upper\n", letter);
                 num = ((letter -65)+ k) % 26; 
-                printf("39 .num is : %d\n", num);
+                // printf("39 .num is : %d\n", num);
                 c = num + 65;
                 printf("41 .char is %c\n  ", c);
             }
-            else{
-            printf("44 .%c is NOT upper\n", letter);
+            else 
+            {
+            // printf("44 .%c is NOT upper\n", letter);
                 num = ((letter -97)+ k) % 26; 
-                printf("46 .num is : %d\n", num);
+                // printf("46 .num is : %d\n", num);
                 c = num + 97;
-                printf("48. char is %c\n  ", c);   
+                printf("48. char is %c\n  ", c);  
+                // ciphertext = c;
+                
             }
+            // printf("ciphertext is %s\n", ciphertext);
         }
         else{
             printf("52. %c is NOT a letter \n", message[i]);
         }
         
         // printf("lower case %c\n", message[i]);
+        
+
     } 
+    printf("\n");
     return 0;
 }
 
