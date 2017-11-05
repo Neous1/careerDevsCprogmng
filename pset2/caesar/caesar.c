96edc8471869930c58bbc81090ce3ada95170207
Neous1 <Neous1@users.noreply.github.com>
2017-11-05 11:51:03 +0000

    add pseudocode for caesar

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -5,86 +5,58 @@
 
 // string message;
 
-string isalpha();
-char isupper();
+
 int main(int argc, string argv[])
 {
+    string isalpha();
+    char isupper();
+    char islower();
+    
     if (argc != 2){
         printf("Please provide a positive number\n");
     }
     string code = argv[1];
     int k = atoi(code);
     
-    // char c; 
-    // int num;
-    char letter ;
-    string ciphertext =" ";
+    char c; 
+    int num;
+    char letter;
+    // string ciphertext =" ";
     printf("15 .The code is:  %d\n", k);
         printf("What is the message: ");
     string message = get_string();
     // printf("%s\n", message);
-    for(int i = 0, len = strlen(message);i < len; i++){
-        // printf("25 . %c\n", message[i]);
-            letter = message[i];
-        if(!isalpha(letter))
+    for(int i = 0, len = strlen(message);i < len; i++)
+    {
+        printf("character @ index %d is : %c\n", i,message[i]);  
+        letter = message[i];
+            
+        if(isalpha(message[i]))
         {
-            ciphertext += letter;
-            printf("31. %s\n", ciphertext);
-        }
-            /*
-        if (isalpha(message[i]))
-        {
-            printf("28 .%c is a letter\n", message[i]);
+            printf("34 .%c is a letter\n", message[i]);
             if(isupper(letter))
             {
-                printf("32 . %c  is upper\n", letter);
+                printf("37 . %c  is upper\n", letter);
                 num = ((letter -65)+ k) % 26; 
-                printf("34 .num is : %d\n", num);
+                printf("39 .num is : %d\n", num);
                 c = num + 65;
-                printf("37 .char is %c\n  ", c);
+                printf("41 .char is %c\n  ", c);
             }
-            else
-            printf("40 .%c is NOT upper\n", letter);
+            else{
+            printf("44 .%c is NOT upper\n", letter);
                 num = ((letter -97)+ k) % 26; 
-                printf("42 .num is : %d\n", num);
+                printf("46 .num is : %d\n", num);
                 c = num + 97;
-                printf("44. char is %c\n  ", c);            
+                printf("48. char is %c\n  ", c);   
+            }
         }
         else{
-            printf("43. %c is NOT a letter \n", message[i]);
-        }*/
+            printf("52. %c is NOT a letter \n", message[i]);
+        }
+        
+
+        // printf("lower case %c\n", message[i]);
     } 
-    // return 0;
+    return 0;
 }
 
-
-/*
-Get the key
-if argc != 2 => throw error
-
-string code = argv[1]
-int k = atoi(code)
-
-get the plaintext
-    get_string()
-encipher
-    one character
-    entire plaintext
-print ciphertext
-
-
-
-
-get key form command line argument
-run key intor integer
-prompt for plaintextr
-for each character in the plaintext string
-    if alphabetic 
-            isalpha()
-            isupper()
-            islower()
-        preserve case
-        shift plaintext character by key
-print ciphertext
-
-*/
