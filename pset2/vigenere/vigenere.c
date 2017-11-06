3dc4e08e41c983c934b2b6e74e1d576f7445d54a
Neous1 <Neous1@users.noreply.github.com>
2017-11-06 15:15:18 +0000

    refactored vigenere

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -7,41 +7,105 @@
 int main(int argc, string argv[])
 {
 
+    // int team;
         if (argc != 2 ){
         printf("missing a parameter\n");
         return 1;
     }
     string key = argv[1];
-    for(int i = 0, len = strlen(key); i < len ; i++){
-        if(!isalpha(key[i])){
+    for(int k = 0, klen = strlen(key); k < klen ; k++){
+        if(!isalpha(key[k])){
             printf("code must me in letters only\n");
             return 1;
         }
+        // printf("%d\n", key[k]);
+        // team = key[k] % klen;
+        // printf("key %d is on team %d\n",key[k], team );
     }
+    printf("Plaintext: ");
+    string plaintext = get_string();
+    // printf("%s\n", plaintext);
+    printf("ciphertext: ");
     
-    
-    // printf("argv is %s\n", argv[1]);
-    // // string code = argv[1];
-    // if(isalpha(argv[1])){
-    //     printf("Good to go!");
-    // }
+    // char c; 
+    // int num, 
+    int knum;
+    char letter;
+    int klen = strlen(key);
+    for (int i = 0, j = 0, plen = strlen(plaintext); i < plen; i++)
+    {
+        // printf("character @ index %d is : %c\n", i,plaintext[i]);  
+        letter = plaintext[i];
+            
+        if(isalpha(letter)) // check if character is a letter and upper case
+        {
+            if( isupper(letter))
+            {            
+                // for (int j = 0 ,klen = strlen(key); j < klen ;)
+                // { //klen = strlen(key)
+                    //convert all char to lowercase
+                    key[j] = tolower(key[j]);
+                    //convert each letter to its alphabetical index
+                    knum = (key[j]-97); //% 26) + 97;
+                    // printf("48 . key[j] is %i\n", knum);
+                    
+                    // if(j % klen )//((letter % klen) == (key[j] % klen))
+                    // {
+                    /*
+                    determine the Alphabetical index of the message[i] (0-25)
+                    k is the number passed in command line
+                    mod 26 insures a wrap around from Z to A
+                    */
+                    letter += knum;
+                    j++;
+                    j = j % klen;
+                    // num = ((letter -65)+ knum) % 26;  // 
+                    // printf("39 .num is : %d\n", num);
+                    // c = num + 65;
+                    // printf("56 . %c becomes %c  ", letter , c);
+                    // printf("61 . %c",c);   
+                    printf("%c", letter);
+                    // }
+                    // break;
+                // }
+            }
+            if( islower(letter))
+            {            
+                // for (int j = 0 ,klen = strlen(key); j < klen ; j++)
+                // { //klen = strlen(key)
+                    //convert all char to lowercase
+                    key[j] = tolower(key[j]);
+                    //convert each letter to its alphabetical index
+                    knum = (key[j]-97);// % 26;
+                    // printf("key[j] is %i\n", knum);
+                    
+                    // if((letter % klen) == (key[j] % klen))
+                    // {
+                    /*
+                    determine the Alphabetical index of the message[i] (0-25)
+                    k is the number passed in command line
+                    mod 26 insures a wrap around from Z to A
+                    */
+                    letter += knum;
+                    j++;
+                    j = j % klen;
+                    // num = ((letter -97)+ knum) % 26;  // 
+                    // // printf("39 .num is : %d\n", num);
+                    // c = num + 97;
+                    // // printf("56 . %c is %c  ", letter , c);
+                    // printf("%c",c);                        
+                    // }
+                    printf("%c", letter);
+                // }
+            }              
+
+        }
+        printf("%c\n",letter);
+        
+        // break;
+    }
 
 
-    // printf("%s\n", code);
-
-    
-        // printf("Plaintext: ");
-        // string plaintext = get_string();
-        // printf("%s\n", plaintext);
-        // printf("ciphertext: ");
-        // for(int i = 0, len = strlen(plaintext);i < len; i++)
-        // {
-        //     printf("%i\n", plaintext[i]);
-        // }
-    
-      
-    
-    
 
     // char c; 
     // int num;
