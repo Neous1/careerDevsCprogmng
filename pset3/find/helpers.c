867d8b10b969015e7c0976d537a005c43d314c34
Neous1 <Neous1@users.noreply.github.com>
2017-11-08 16:01:47 +0000

    add code to helpers.c to find needle

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -5,7 +5,7 @@
  */
  
 #include <cs50.h>
-
+#include <stdio.h>
 #include "helpers.h"
 
 /**
@@ -13,7 +13,43 @@
  */
 bool search(int value, int values[], int n)
 {
+    //value => int needle = atoi(argv[1]);
+    //value[] => int haystack[MAX]
+    // n => int size;
+    
+    for (int i = 0; i<n; i++){
+        if ( value == values[i]){
+            return true;
+        }
+        // else
+        //     return false;
+        
+    }
+    
+    
     // TODO: implement a searching algorithm
+    /*
+    Search
+        return true if value is fond in haystack
+        return false if value is not in haystack
+        
+    Linear search: 
+        O(n) -> slow
+        Can search any list
+    binary search
+        O(log n) -> fast
+        can only search sorted lists
+        */
+        
+        /*
+        
+    Get a list 
+    sort the list 
+    apply binary search
+    
+    
+        
+        */
     return false;
 }
 
